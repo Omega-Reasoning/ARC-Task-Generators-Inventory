180010288255fb0c6c0fb7a9ef70fc324fdc979e
Jens Lehmann <jlehmnn@amazon.com>
2024-12-30 09:55:50 +0100

    draft of new raster logic ARC task

--- input_library.py
+++ input_library.py
@@ -1,7 +1,7 @@
 """
-Input Matrix Generation Library
+Input Grid Generation Library
 
-This module provides a collection of utility functions for creating ARC matrices. 
+This module provides a collection of utility functions for creating ARC grids. 
 They can be used in the create_input() of the ARCTaskGenerator class, but not in
 the transform_input() method. In other words, those functions are not relevant
 for solving ARC tasks at runtime.
